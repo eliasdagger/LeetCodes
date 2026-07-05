2439c92bf1335868cf449436fd508192b979faac
eliasdagger <125106050+eliasdagger@users.noreply.github.com>
2026-07-05 17:04:07 -0400

    note

--- Py/strStr.py
+++ Py/strStr.py
@@ -3,6 +3,7 @@ class Solution(object):
         # starting pointer
         l = 0
         # utilise a sliding window approach, window should include l as a starting point, then +length of needle, final value exclusive
+        # <= since the window is l + len exclusive final elementthus, no index out of bounds error
         while l + len(needle) <= len(haystack):
             # if this window is the needle, l will return as the starting index of the needle, else we will shift the window.
             if haystack[l:l+len(needle)] == needle:
