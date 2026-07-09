3e2e6db4a2e29f732fd2d81f5779216e14aefabd
Elie Dagher <125106050+eliasdagger@users.noreply.github.com>
2026-07-08 20:44:43 -0400

    judegeCircle explanation

--- Py/judgeCircle.py
+++ Py/judgeCircle.py
@@ -1,3 +1,4 @@
 class Solution(object):
     def judgeCircle(self, moves):
+        # if opposite direction moves are made equally they cancel eachother out thus we are still at the centre. 
         return moves.count("U") == moves.count("D") and moves.count("L") == moves.count("R")
