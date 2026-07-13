8695f8e36f2885d937dd5df44ebd2d4e75a1ceec
eliasdagger <125106050+eliasdagger@users.noreply.github.com>
2026-07-13 09:30:18 -0400

    reverseList note

--- Java/reverseList.java
+++ Java/reverseList.java
@@ -1,30 +1,15 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode curr, prev, n;
-        curr = head;
-        prev = null;
-        if (curr == null){
-            return head;
-        }
-        n = curr.next;
-        while (n != null){
-            curr.next = prev;
-            prev = curr;
-            curr = n;
+        ListNode succ, curr, n;
+        succ = null;
+        curr = n = head;
+        // set next to curr next since we will use a three pointer technique which will disconnect the link to the rest of the list, set curr to previous node then then shift succ to curr and curr to next, continue, this will reverse the list. Condition is while curr is not null bc this will iterate once more leaving succ as the last node, thus every node is reversed.
+        while (curr != null){
             n = curr.next;
+            curr.next = succ;
+            succ = curr;
+            curr = n;
         }
-        curr.next = prev;
-        head = curr;
-        return head;
+
+        return succ;
     }
-}
